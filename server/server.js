a3f5a49454350ae819998e99e5520c83f3137988
26nch <nchb1807838@gmail.com>
2025-02-20 07:13:20 -0800

    add(/server): add folder config, routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log("server running on port 2013")
+import express from 'express';
+import { PORT } from './config/env.js';
+import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js'
+
+const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Hai Nguyen!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+export default app;
