b538041b6093be4b750e1a3c44f41d31c16e8de6
26nch <nchb1807838@gmail.com>
2025-02-25 06:27:19 -0800

    add(src/): components fol and index.css

---
+++ client/src/components/register/Register.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './register.css'
+
+const Register = () => {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <br/>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <input type="text" placeholder='Name'/>
+            </div>
+            <div className="input">
+                <input type="email" placeholder='Email'/>
+            </div>
+            <div className="input">
+                <input type="password" placeholder='Password'/>
+            </div>
+        </div>
+        <div className="forgot-password">Forgot Password! <span>Click Here!</span></div>
+        <div className="submit-container">
+            <div className="submit">Register</div>
+            <div className="submit">Login</div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
