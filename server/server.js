8570249291eed7f0b87757b57d11139be016bb90
26nch <nchb1807838@gmail.com>
2025-02-26 07:08:25 -0800

    config(/server): connect to database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { PORT } from './config/env.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js'
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
@@ -12,8 +13,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to Hai Nguyen!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
     console.log(`Server running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
 
 export default app;
