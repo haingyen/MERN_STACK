2aa76461a4df87a960c7da7c2e847e847c88c7d9
26nch <nchb1807838@gmail.com>
2025-02-28 07:25:21 -0800

    mod(/): mod server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+// import cookieParse from 'cookie-parser';
 import { PORT } from './config/env.js';
 import authRouter from './routes/auth.routes.js';
-import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js';
+import blogRouter from './routes/blog.routes.js';
 import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// app.use(cookieParse());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/blog', blogRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Hai Nguyen!');
